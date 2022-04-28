cc589524e98ae5e0eaab08ffc20d9d58ba1cb496
anshu <anshusharma6327@gmail.com>
2022-04-28 21:56:07 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Routes, Route, Switch } from "react-router-dom";
-import Home from './components/Home'
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AboutUs, OurAim, OurVision } from "./pages/AboutUs";
+import {
+Services,
+ServicesOne,
+ServicesTwo,
+ServicesThree,
+} from "./pages/Services";
+import { Events, EventsOne, EventsTwo } from "./pages/Events";
+import Contact from "./pages/ContactUs";
+import Support from "./pages/Support";
 function App() {
-  return (
-    <>
-     <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </>
-
-  )
+return (
+	<Router>
+	<Sidebar />
+	<Switch>
+		<Route path="/about-us" exact component={AboutUs} />
+		<Route path="/about-us/aim" exact component={OurAim} />
+		<Route path="/about-us/vision" exact component={OurVision} />
+		<Route path="/services" exact component={Services} />
+		<Route path="/services/services1" exact component={ServicesOne} />
+		<Route path="/services/services2" exact component={ServicesTwo} />
+		<Route path="/services/services3" exact component={ServicesThree} />
+		<Route path="/contact" exact component={Contact} />
+		<Route path="/events" exact component={Events} />
+		<Route path="/events/events1" exact component={EventsOne} />
+		<Route path="/events/events2" exact component={EventsTwo} />
+		<Route path="/support" exact component={Support} />
+	</Switch>
+	</Router>
+);
 }
 
-export default App
+export default App;
